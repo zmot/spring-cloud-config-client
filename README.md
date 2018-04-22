0ab94ef0456cf899eb511d26c9cb26e2fea1de08
zmot <tomaszmuc@gmail.com>
2018-04-22 18:54:39 +0200

    basic spring cloud bus usage for configuration refresh

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Sample application using Spring Cloud Config server <http://http://localhost:809
 
 Exposes an endpoint at <http://localhost:8080/lucky-word>
 
+## Dynamic configuration refresh
+Requires RabbitMQ server and running. 
+
+In order to refresh the configuration of Lucky Client service you need to make a POST request to Spring Cloud Configuration server, i.e.:
+<http://localhost:8090/bus/refresh>
--- src/main/java/com/tm/microservices/config/client/LuckyClientController.java
+++ src/main/java/com/tm/microservices/config/client/LuckyClientController.java
@@ -1,20 +1,29 @@
 package com.tm.microservices.config.client;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ConfigurationProperties("lucky")
 public class LuckyClientController {
 
-    @Value(value = "${lucky.word}")
-    private String luckyWord;
+    private String word;
 
     @RequestMapping(name = "/lucky-word", method = RequestMethod.GET)
     @ResponseBody
     public String luckyWord() {
-        return "My lucky word is: " + luckyWord;
+        return "My lucky word is: " + word;
     }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
 }
